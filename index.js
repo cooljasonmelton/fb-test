85c2666e9b488c1d14454919e2649863f4dc6faf
cooljasonmelton <cooljasonmelton>
2020-08-17 16:16:10 -0500

    testing videos from fb

---
+++ index.js
@@ -0,0 +1,15 @@
+// download button
+const downloadBtn = document.getElementById('downloader')
+// video player
+const videoPlayer = document.getElementById('video')
+// video source
+const videoSrc = document.getElementById('video-src')
+
+
+
+
+
+downloadBtn.addEventListener('click', e => {
+
+})
+
